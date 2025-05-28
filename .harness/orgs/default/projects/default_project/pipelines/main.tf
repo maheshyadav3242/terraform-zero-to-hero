8bb5056a65eec0bcadebeecb40e1ae5de753f7b1
mahesh yadav <59454115+maheshyadav3242@users.noreply.github.com>
2025-05-28 16:02:47 +0530

    Update main.tf

--- .harness/orgs/default/projects/default_project/pipelines/main.tf
+++ .harness/orgs/default/projects/default_project/pipelines/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-1"
 }
 
-module "ec2_instance" {
+  resource "aws_instance" "ec2_instance" {
   ami_value = "ami-084568db4383264d4" # replace this
   instance_type_value = "t2.micro"
   subnet_id_value = "subnet-043a5c414a091c43b"
