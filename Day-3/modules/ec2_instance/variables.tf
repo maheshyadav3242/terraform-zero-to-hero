3eb90345d6a85aec14677f1cc7397cc5066a2004
mahesh yadav <59454115+maheshyadav3242@users.noreply.github.com>
2025-05-28 16:14:24 +0530

    Update variables.tf

--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,12 +1,20 @@
+
 variable "ami_value" {
     description = "value for the ami"
+    default = "
 }
 
 variable "instance_type_value" {
     description = "value for instance_type"
+    default = "t2.micro"
 }
 
 variable "subnet_id_value" {
     description = "value for the subnet_id"
     default = "subnet-05e518d24c7b55b1f"
+}
+
+variable "vpc_id_value" {
+    description = "value for the subnet_id"
+    default = "vpc-0f2360d511fdab503"
 }
